0f2f244434d1d6edf07b8f89b971ca1770a59988
Arthur Rocha <arthur9869@gmail.com>
2019-11-19 11:41:21 -0300

    Initial commit.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_app_chat.Models;
+using web_app_chat.Repositories;
+
+namespace web_app_chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly static ConnectionsRepository _connections = new ConnectionsRepository();
+
+        // Retorna lista de usuário no chat e usuário que acabou de logar
+
+        public override Task OnConnectedAsync()
+        {
+            var user = JsonConvert.DeserializeObject<User>(Context.GetHttpContext().Request.Query["user"]);
+            _connections.Add(Context.ConnectionId, user);
+            Clients.All.SendAsync("chat", _connections.GetUsers(), user);
+            return base.OnConnectedAsync();
+        }
+
+        // Método responsável por encaminhar as mensagens pelo hub
+        public async Task SendMessage(ChatMessage chat)
+        {
+            //Ao usar o método Client(_connections.GetUserId(chat.destination)) eu estou enviando a mensagem apenas para o usuário destino, não realizando broadcast
+            await Clients.Client(_connections.GetUserById(chat.destination)).SendAsync("Receive", chat.sender, chat.message);
+        }
+    }
+}
---
+++ Repositories/ConnectionsRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_app_chat.Models;
+
+namespace web_app_chat.Repositories
+{
+    public class ConnectionsRepository
+    {
+        private readonly Dictionary<string, User> connections = new Dictionary<string, User>();
+
+        public void Add(string id, User user)
+        {
+            if (!connections.ContainsKey(id))
+            {
+                connections.Add(id, user);
+            }
+        }
+
+        public string GetUserById(long id)
+        {
+            return (from con in connections
+                    where con.Value.key == id
+                    select con.Key).FirstOrDefault();
+        }
+
+        public List<User> GetUsers()
+        {
+            return (from con in connections
+                    select con.Value).ToList();
+        }
+    }
+}
